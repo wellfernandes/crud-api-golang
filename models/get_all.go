8ea2f48028acf01b472595a700a31dc6a7416cbc
Welliton Fernandes Leal <wellfernandes@me.com>
2022-11-01 11:36:58 -0300

    added get all (select all) package

---
+++ models/get_all.go
@@ -0,0 +1,27 @@
+package models
+
+import "crud-api-golang/db"
+
+func GetAll() (todos []Todo, err error) {
+
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	rows, err := conn.Query(`SELECT * FROM todos`)
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+		if err != nil {
+			continue //if there is an error, it will ignore and go to the next one *just for learning
+		}
+		todos = append(todos, todo)
+	}
+	return
+}
