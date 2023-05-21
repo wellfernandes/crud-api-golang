8797d61e0ca02cde51823ae1e0ff52f1a44a0e7a
Welliton Fernandes Leal <wellfernandes@me.com>
2023-05-21 20:38:54 -0300

    changed main to load env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"crud-api-golang/configs"
 	"crud-api-golang/rest"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	configs.Load()
+
+	port, host, database, err := configs.GetConfigInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(
+		"\n\nAPI Port:", port,
+		"\nHost:", host,
+		"\nDatabase:", database,
+	)
 
 	r := rest.NewServerRouter()
 
