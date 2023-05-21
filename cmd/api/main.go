b559030628d97b4959bcf507dae080838a05f1ef
Welliton Fernandes Leal <wellfernandes@me.com>
2023-05-21 20:56:50 -0300

    changed static port number to configured port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crud-api-golang/configs"
 	"crud-api-golang/rest"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,19 +12,20 @@ func main() {
 
 	configs.Load()
 
-	port, host, database, err := configs.GetConfigInfo()
+	port, host, database, strConn, err := configs.GetConfigInfo()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println(
-		"\n\nAPI Port:", port,
+		"\n\nPort:", port,
 		"\nHost:", host,
-		"\nDatabase:", database,
+		"\nDatabase name:", database,
+		"\nString connection database:", strConn,
 	)
 
 	r := rest.NewServerRouter()
 
-	log.Println("running api...")
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Println("\n\nAPI is running...")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
 }
