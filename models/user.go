055007ada422e1e3f6551130fdeb3b2e058070e9
Welliton Fernandes Leal <wellfernandes@me.com>
2023-05-21 20:41:31 -0300

    added user model

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+// User is a user entity
+type User struct {
+	ID       uint64    `json:"id,omitempty"`
+	Name     string    `json:"name,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Password string    `json:"password,omitempty"`
+	CreateAt time.Time `json:"create_at,omitempty"`
+}
