d9762d02695d1ac7e01484f142348725a09e62f1
Welliton Fernandes Leal <wellfernandes@me.com>
2023-05-21 21:55:52 -0300

    changed user controller to test

--- internal/controller/user.go
+++ internal/controller/user.go
@@ -1,32 +1,64 @@
 package controller
 
-import "net/http"
+import (
+	"crud-api-golang/database"
+	"crud-api-golang/internal/repository"
+	"crud-api-golang/models"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-// Create a new user
+// Create receives a request to create a user
 func Create(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("Create a new user"))
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err) // test
+	}
+
+	var user models.User
+
+	err = json.Unmarshal(requestBody, &user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.NewConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repository.NewUserRepository(db)
+	userIDCreated, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err) // test
+	}
+
+	w.Write([]byte(fmt.Sprintf("user created: %d", userIDCreated)))
 }
 
-// GetList get all users
+// GetList receives a request to get all a users
 func GetList(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Get all users"))
 }
 
-// GetByID get a user
+// GetByID receives a request to get a user
 func GetByID(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Get a user"))
 }
 
-// Update update a user
+// Update receives a request to update a user
 func Update(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Update a user"))
 }
 
-// Delete update a user
+// Delete receives a request to delete a user
 func Delete(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Delete a user"))
