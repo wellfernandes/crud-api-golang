d2b26e5a772e750994fe3b2c8cf2f9d834f5df7e
Welliton Fernandes Leal <wellfernandes@me.com>
2023-05-23 20:52:09 -0300

    added check fields and prepare user func

--- models/user.go
+++ models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User is a user entity
 type User struct {
@@ -10,4 +14,42 @@ type User struct {
 	Email    string    `json:"email,omitempty"`
 	Password string    `json:"password,omitempty"`
 	CreateAt time.Time `json:"create_at,omitempty"`
+}
+
+// PrepareUser prepare the user model to insert into the database
+func (u *User) PrepareUser() error {
+
+	err := u.checkFields()
+	if err != nil {
+		return err
+	}
+
+	u.formatFields()
+
+	return nil
+}
+
+// checkFields checks fields that cannot be null
+func (u *User) checkFields() error {
+	if u.Name == "" {
+		return errors.New("the name field cannot be empty")
+	}
+	if u.Nick == "" {
+		return errors.New("the nick field cannot be empty")
+	}
+	if u.Email == "" {
+		return errors.New("the email field cannot be empty")
+	}
+	if u.Password == "" {
+		return errors.New("the password field cannot be empty")
+	}
+
+	return nil
+}
+
+// formatFields formats filled fields by removing spaces from the ends
+func (u *User) formatFields() {
+	u.Name = strings.TrimSpace(u.Name)
+	u.Nick = strings.TrimSpace(u.Nick)
+	u.Email = strings.TrimSpace(u.Email)
 }
