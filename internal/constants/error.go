a5209ca6db88274d9112589c880f9ae526263ae3
Welliton Fernandes Leal <wellfernandes@me.com>
2023-05-27 14:08:26 -0300

    added constants error

---
+++ internal/constants/error.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	ERROR_EMPTY_NAME     = "the name field cannot be empty"
+	ERROR_EMPTY_NICK     = "the nick field cannot be empty"
+	ERROR_EMPTY_EMAIL    = "the email field cannot be empty"
+	ERROR_EMPTY_PASSWORD = "the password field cannot be empty"
+)
