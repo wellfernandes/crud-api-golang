f09db0c320ab2cf41e24940fe92494003e1e4f1c
Welliton Fernandes Leal <wellfernandes@me.com>
2023-06-03 20:55:12 -0300

    added new constant error

--- internal/constants/error.go
+++ internal/constants/error.go
@@ -1,8 +1,9 @@
 package constants
 
 const (
-	ERROR_EMPTY_NAME     = "the name field cannot be empty"
-	ERROR_EMPTY_NICK     = "the nick field cannot be empty"
-	ERROR_EMPTY_EMAIL    = "the email field cannot be empty"
-	ERROR_EMPTY_PASSWORD = "the password field cannot be empty"
+	ERROR_EMPTY_NAME           = "the name field cannot be empty"
+	ERROR_EMPTY_NICK           = "the nick field cannot be empty"
+	ERROR_EMPTY_EMAIL          = "the email field cannot be empty"
+	ERROR_EMPTY_PASSWORD       = "the password field cannot be empty"
+	ERROR_ENVIROMENT_VARIABLES = "error in environment variables"
 )
