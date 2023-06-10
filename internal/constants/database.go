f6562542d529a4d3ed81d9b537de0db8004ecb66
Welliton Fernandes Leal <wellfernandes@me.com>
2023-06-10 20:54:15 -0300

    added str connection constant

--- internal/constants/database.go
+++ internal/constants/database.go
@@ -2,6 +2,7 @@ package constants
 
 const (
 	MYSQL_DRIVER      = "mysql"
+	STRING_CONNECTION = "%s:%s@/%s?charset=utf8&parseTime=True&loc=Local"
 	DATABASE_HOST     = "DATABASE_HOST"
 	DATABASE_USER     = "DATABASE_USER"
 	DATABASE_PASSWORD = "DATABASE_PASSWORD"
